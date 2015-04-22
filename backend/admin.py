7390213d10c258e7d207d7e86e2b97aee45516e0
“Jack <yellowponys@gmail.com>
2015-04-22 19:44:49 +1000

    manually put in user admin look, added added card
    yay

--- backend/admin.py
+++ backend/admin.py
@@ -3,11 +3,10 @@ admin.AdminSite.site_header = 'Django Rechained'
 admin.AdminSite.site_title = 'Django Rechained'
 admin.AdminSite.index_title = 'Wow your cool'
 
-
 from .models import *
-class CardAdmin(admin.ModelAdmin):
-    list_display = ('content', 'author', 'date_created')
 
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('content', 'author', 'votes', 'uses', 'date_created')
 
 admin.site.register(Answer, CardAdmin)
 admin.site.register(Question, CardAdmin)
@@ -17,6 +16,19 @@ from .models import User
 
 class UserAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ('email_valid',)
-    fieldsets = None
+    fieldsets = (
+            (None, {
+                'fields': ('username', 'password')
+            }),
+            ('Personal Info', {
+                'fields': ('first_name', 'last_name', ('email', 'email_valid'))
+            }),
+            ('Permissions', {
+                'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+            }),
+            ('Important Dates', {
+                'fields': ('last_login', 'date_joined')
+            }),
+    )
 
 admin.site.register(User, UserAdmin)
