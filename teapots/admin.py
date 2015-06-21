42e3fecd1bde8da447f8b24935a9aed92a716926
“Jack <yellowponys@gmail.com>
2015-06-21 17:05:19 +1000

    beautify?

--- teapots/admin.py
+++ teapots/admin.py
@@ -6,6 +6,7 @@ admin.AdminSite.index_title = 'Wow your cool'
 
 from .models import *
 
+
 class CardAdmin(admin.ModelAdmin):
     list_display = ('content', 'author', 'votes', 'uses', 'date_created')
 
@@ -14,21 +15,22 @@ admin.site.register(Question, CardAdmin)
 
 from django.contrib.auth.admin import UserAdmin
 
+
 class UserAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ('email_valid',)
     fieldsets = (
-            (None, {
-                'fields': ('username', 'password')
-            }),
-            ('Personal Info', {
-                'fields': ('first_name', 'last_name', ('email', 'email_valid'))
-            }),
-            ('Permissions', {
-                'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
-            }),
-            ('Important Dates', {
-                'fields': ('last_login', 'date_joined')
-            }),
+        (None, {
+            'fields': ('username', 'password')
+        }),
+        ('Personal Info', {
+            'fields': ('first_name', 'last_name', ('email', 'email_valid'))
+        }),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+        }),
+        ('Important Dates', {
+            'fields': ('last_login', 'date_joined')
+        }),
     )
 
 admin.site.register(User, UserAdmin)
--- teapots/models.py
+++ teapots/models.py
@@ -1,24 +1,30 @@
 from django.db import models
 from django.conf import settings
 
+
 class Card(models.Model):
-	author = models.ForeignKey(settings.AUTH_USER_MODEL)
-	date_created = models.DateTimeField('date created', auto_now_add=True)
-	uses = models.IntegerField(default=0)
-	votes = models.IntegerField(default=0)
-	def __str__(self):
-		return self.content
-	class Meta:
-		abstract = True
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    date_created = models.DateTimeField('date created', auto_now_add=True)
+    uses = models.IntegerField(default=0)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.content
+
+    class Meta:
+        abstract = True
+
 
 class Question(Card):
-	content = models.CharField(max_length=150)
+    content = models.CharField(max_length=150)
+
 
 class Answer(Card):
-	owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='owner')
-	content = models.CharField(max_length=50)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='owner')
+    content = models.CharField(max_length=50)
 
 from django.contrib.auth.models import AbstractUser
+
 
 class User(AbstractUser):
     email_valid = models.BooleanField('email verified', default=False)
