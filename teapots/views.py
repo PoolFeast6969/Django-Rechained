77c511c8b1eeb316b1bb7eed4eb7c5fe301c1001
“Jack <yellowponys@gmail.com>
2015-08-06 17:12:21 +1000

    idk something about login

--- teapots/views.py
+++ teapots/views.py
@@ -6,15 +6,6 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.core.exceptions import ValidationError
 
 def index(request):
-    if request.POST:
-        form = AuthenticationForm(data=request.POST)
-        try:
-            if form.is_valid():
-                login(request,form.get_user())
-            else:
-                HttpResponse('authentication failed')
-        except ValidationError:
-             HttpResponse("Authentication failed - wrong password/login or user is not active or can't set cookies")
     questions = Question.objects.all()
     context = {'questions': questions}
     return render(request, 'questions.html', context)
