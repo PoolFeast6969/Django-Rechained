9eae387689a1671bcf86db9fbde25ef7257179c1
“Jack <yellowponys@gmail.com>
2015-08-08 15:59:14 +1000

    actually did nothing

--- teapots/urls.py
+++ teapots/urls.py
@@ -6,6 +6,6 @@ from teapots import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^login/$', auth_views.login,{'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
 ]
--- teapots/views.py
+++ teapots/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.signals import user_logged_in
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 
 def index(request):
     questions = Question.objects.all()
