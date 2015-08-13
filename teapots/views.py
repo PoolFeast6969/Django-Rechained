5e70ebfc470352b69c53686726b569afc6224777
“Jack <yellowponys@gmail.com>
2015-08-13 17:21:02 +1000

    got login messages to actually check if your logged in

--- teapots/views.py
+++ teapots/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.models import User as user
 from django.contrib.auth.signals import user_logged_in
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
@@ -12,9 +13,17 @@ def index(request):
     return render(request, 'questions.html', context)
 
 def login(request, template_name='login.html'):
-    messages.add_message(request, messages.INFO, "logged in")
-    return auth_views.login(request, template_name)
+    response = auth_views.login(request, template_name)
+    if request.user.is_authenticated():
+        messages.info(request, 'logged in')
+    else:
+        messages.error(request, 'there was a problem with your login')
+    return response
 
 def logout(request, next_page='/'):
-    messages.add_message(request, messages.INFO, "logged out")
-    return auth_views.logout(request, next_page)
+    response = auth_views.logout(request, next_page)
+    if request.user.is_authenticated():
+        messages.error(request, 'there was a problem with your logout.....somehow')
+    else:
+        messages.info(request, 'logged out')
+    return response
