096afe5babc4cb29e73199c69395848f563c1c47
“Jack <yellowponys@gmail.com>
2015-08-19 16:57:52 +1000

    capitalisation and next page for logout

--- teapots/views.py
+++ teapots/views.py
@@ -2,10 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
 from django.contrib.auth import views as auth_views
-from django.contrib.auth.models import User as user
-from django.contrib.auth.signals import user_logged_in
 from django.contrib import messages
-from django.contrib.messages.views import SuccessMessageMixin
 
 def index(request):
     questions = Question.objects.all()
@@ -15,15 +12,15 @@ def index(request):
 def login(request, template_name='login.html'):
     response = auth_views.login(request, template_name)
     if request.user.is_authenticated():
-        messages.info(request, 'logged in')
+        messages.info(request, 'Logged In')
     else:
-        messages.error(request, 'there was a problem with your login')
+        messages.error(request, 'There was a problem with your login')
     return response
 
 def logout(request, next_page='/'):
     response = auth_views.logout(request, next_page)
     if request.user.is_authenticated():
-        messages.error(request, 'there was a problem with your logout.....somehow')
+        messages.error(request, 'There was a problem with your logout.....Somehow')
     else:
-        messages.info(request, 'logged out')
+        messages.info(request, 'Logged Out')
     return response
